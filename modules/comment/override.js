c080dd18af5e5649a07d54c7591a175015179e21
nebel <nebel08@gmail.com>
2014-11-06 17:59:20 +0800

    sence init

---
+++ modules/comment/override.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+exports.data = [
+//    {view: 'home/index', controller: 'mod', action:'index1', mode:'replace', priority:1},
+//    {view: 'home/index', controller: 'mod', action:'index', mode:'append', priority:2},
+];
+
+//append - Append data processed from controller to the view
+//prepend - Prepend data processed from controller to the view
+//replace - Replace data processed from controller with view
